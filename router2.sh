7354b5993f55917e70e81c3cfabfff1d54b03ddc
Sergio Povoli <sergio.povoli@studenti.unitn.it>
2018-11-19 11:28:27 +0100

    fix bug router1.sh and router2.sh

--- router2.sh
+++ router2.sh
@@ -5,8 +5,8 @@ wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-k
 add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
 apt-get update
 apt-get install -y frr --assume-yes --force-yes
+sysctl net.ipv4.ip_forward=1
 ip link set dev eth1 up
 ip link set dev eth2 up
-sysctl net.ipv4.ip_forward=1
 ip add add 192.168.251.2/30 dev eth2
 ip add add 192.168.252.1/30 dev eth1
