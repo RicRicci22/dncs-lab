dc49a7d7772263d9fe6d36993f78fd010150feda
Sergio Povoli <sergio.povoli@studenti.unitn.it>
2018-11-19 14:19:14 +0100

    Add route host1a and host1c to router

--- host1a.sh
+++ host1a.sh
@@ -3,3 +3,4 @@ apt-get update
 apt-get install -y tcpdump --assume-yes
 ip link set dev eth1 up
 ip add add 192.168.249.2/24 dev eth1
+ip route add 192.168.248.0/21 via 192.168.249.1
--- host1b.sh
+++ host1b.sh
@@ -6,3 +6,4 @@ apt-get update
 apt-get install -y docker-ce --assume-yes --force-yes
 ip link set dev eth1 up
 ip add add 192.168.250.2/27 dev eth1
+ip route add 192.168.248.0/21 via 192.168.250.1
