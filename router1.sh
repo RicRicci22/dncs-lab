0d83b656914d3a7dbbd9847bb8fe151cdbf42499
Sergio Povoli <sergio.povoli@studenti.unitn.it>
2018-11-19 09:01:23 +0100

    add first command in router1-2 and host2c

--- router1.sh
+++ router1.sh
@@ -5,3 +5,10 @@ wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-k
 add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
 apt-get update
 apt-get install -y frr --assume-yes --force-yes
+ip link set dev eth1 up
+ip link add link eth1 name eth1.11 type vlan id 11
+ip link add link eth1 name eth1.12 type vlan id 12
+ip link set dev eth1.11 up
+ip link set dev eth1.12 up
+sysctl net.ipv4.ip_forward=1
+ip add add 192.168.251.1/30 dev eth2
