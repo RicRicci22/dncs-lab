3badb6c30ed7c8b3726d557bef902bfac334951c
Riccardo <riccardo.ricci-1@studenti.unitn.it>
2018-11-21 11:06:26 +0100

    fix bug on host 2 c on donwloading docker

--- host2c.sh
+++ host2c.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+sudo su 
+apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -9,18 +10,20 @@ ip link set dev eth1 up
 ip add add 192.168.252.2/30 dev eth1
 ip route add 192.168.248.0/21 via 192.168.252.1
 
-
-sudo docker run -dit --name SRwebserver -p 8080:80 -v /home/user/website/:/usr/local/apache2/htdocs/ httpd:2.4
-
+docker run -dit --name SRwebserver -p 8080:80 -v /home/user/website/:/usr/local/apache2/htdocs/ httpd:2.4
 
 echo "<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pagina web di Serghio e Riccardo</title>
+    <title>Pagina web di Sergio e Riccardo</title>
 </head>
 <body>
-    <h1>Hi! This is maneskin yooo!</h1>   
+    <h1 style='color: #5e9ca0;'>HI, THIS IS A DEMO WEBPAGE&nbsp;</h1>
+    <h2 style='color: #2e6c80;'>Creators:</h2>
+	<p>- Riccardo Ricci -- 181398</p>
+	<p>- Sergio Povoli - 185790</p>
+	<p><strong>&nbsp;</strong></p>   
 </body>
 </html>" > /home/user/website/index.html
 
