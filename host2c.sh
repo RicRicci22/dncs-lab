bd16df22f7383d00cdcc7089caa038d0db52127b
Sergio Povoli <sergio.povoli@studenti.unitn.it>
2018-11-21 09:32:14 +0100

    Auto stash before merge of "master" and "origin/master"

--- host2c.sh
+++ host2c.sh
@@ -9,6 +9,9 @@ ip link set dev eth1 up
 ip add add 192.168.252.2/30 dev eth1
 ip route add 192.168.248.0/21 via 192.168.252.1
 sudo docker run -dit --name SRwebserver -p 8080:80 -v /home/user/website/:/usr/local/apache2/htdocs/ httpd:2.4
+
+
+
 vi /home/user/website/paginaweb.html
 #PEr sergio qua comincia il problema
 '<!DOCTYPE html>
